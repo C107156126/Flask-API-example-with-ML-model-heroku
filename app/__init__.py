99b67d06fd90e3b8ef8997a721feada146c71109
C107156126 <76277840+C107156126@users.noreply.github.com>
2021-09-04 14:31:47 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ cols_sql=['Age','Attrition','BusinessTravel','DailyRate','Department','DistanceF
 
 CORS(app)
 def userin():
-     return 'hello!!'
+     return 'hello!!23283y823'
 @app.route('/getdata')
 def getdata():
      tmp=[]
